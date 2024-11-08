1a71f9ba43c2388f272b1039c49dcfb3a4c9c240
Egor-Liadsky <igeg91999@gmail.com>
2024-11-09 00:41:31 +0300

    create simple tests

---
+++ tests/SimpleTest.py
@@ -0,0 +1,14 @@
+def test_check_simple_method_success():
+    expected_data = 3
+    actual_data = plus(1, 2)
+    assert expected_data == actual_data
+
+
+def test_check_simple_method_failure():
+    expected_data = 3
+    actual_data = plus(1, 3)
+    assert expected_data != actual_data
+
+
+def plus(num1: int, num2: int) -> int:
+    return num1 + num2
