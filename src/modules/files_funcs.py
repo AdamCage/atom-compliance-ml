23acde13fc6f2c8ecdc20bf5e5dd3c3f8c0422bf
AdamCage <yytrby@gmail.com>
2024-11-09 18:56:05 +0300

    fix

--- src/modules/files_funcs.py
+++ src/modules/files_funcs.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 from typing import Any
+import joblib
+from datetime import datetime
 
 import yaml
 
@@ -17,3 +19,16 @@ def load_yaml(path: Path) -> dict[str, Any]:
     with open(path, "r") as file:
         return yaml.safe_load(file)
 
+def obj_to_pickle(obj: Any, path: Path) -> None:
+    """Сохраняет объект в файл с помощью joblib.
+
+    Args:
+        obj (Any): Объект, который нужно сохранить.
+        path (Path): Путь к файлу, в который будет сохранен объект.
+    """
+    with open(path, "wb") as file:
+        joblib.dump(obj, file)
+
+
+def create_model_name(model_obj: Any) -> str:
+    return f'{model_obj.__repr__().replace(r"()", "").lower()}_{datetime.today().strftime("%Y-%m-%d")}.pickle'
