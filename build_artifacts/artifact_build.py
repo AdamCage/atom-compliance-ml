26ba46dbf5f3962b6fd431357d5d0c04162241d1
Egor-Liadsky <igeg91999@gmail.com>
2024-11-09 11:53:18 +0300

    create upload artifact

---
+++ build_artifacts/artifact_build.py
@@ -0,0 +1,27 @@
+import os
+import zipfile
+import os
+
+
+def build():
+    # with open("../config/config.yaml") as config:
+    #     version_app = yaml.safe_load(config)["version_app"]
+
+    source_dir = '../../atom-compliance-ml'
+    # output_filename = f'../../atom-compliance-ml-{version_app}-release.zip'
+    output_filename = f'../atom-compliance-ml-test-release.zip'
+
+    with zipfile.ZipFile(output_filename, 'w') as zipf:
+        for root, dirs, files in os.walk(source_dir):
+            if 'venv' in root:
+                continue
+            if '__pycache__' in root:
+                continue
+            for file in files:
+                file_path = os.path.join(root, file)
+                zipf.write(file_path, os.path.relpath(file_path, source_dir))
+    print("File create success")
+
+
+if __name__ == '__main__':
+    build()
