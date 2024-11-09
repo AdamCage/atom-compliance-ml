cb6d263bca7d1ee68e8e4364d8010a022857cfcd
Egor-Liadsky <igeg91999@gmail.com>
2024-11-09 10:20:13 +0300

    create upload artifact

--- build/artifact_build.py
+++ build/artifact_build.py
@@ -1,5 +1,6 @@
 import os
 import zipfile
+import shutil
 
 
 def build():
@@ -8,17 +9,32 @@ def build():
 
     source_dir = '../../atom-compliance-ml'
     # output_filename = f'../../atom-compliance-ml-{version_app}-release.zip'
-    output_filename = f'../atom-compliance-ml-test-release.zip'
+    output_filename = f'../artifacts/atom-compliance-ml-test-release.zip'
 
-    with zipfile.ZipFile(output_filename, 'w') as zipf:
+    # with zipfile.ZipFile(output_filename, 'w') as zipf:
+    #     for root, dirs, files in os.walk(source_dir):
+    #         if 'venv' in root:
+    #             continue
+    #         if '__pycache__' in root:
+    #             continue
+    #         for file in files:
+    #             file_path = os.path.join(root, file)
+    #             zipf.write(file_path, os.path.relpath(file_path, source_dir))
+    source_dir = os.path.abspath('../../atom-compliance-ml')
+    output_dir = os.path.abspath('./artifacts')
+    output_filename = os.path.join(output_dir, f'atom-compliance-ml-test-release.zip')
+
+    os.makedirs(output_dir, exist_ok=True)
+
+    with zipfile.ZipFile(output_filename, 'w', zipfile.ZIP_DEFLATED) as zipf:
         for root, dirs, files in os.walk(source_dir):
-            if 'venv' in root:
+            if any(excluded in root for excluded in {'venv', 'node_modules', 'build', '__pycache__', 'dist'}):
                 continue
-            if '__pycache__' in root:
-                continue
+
             for file in files:
                 file_path = os.path.join(root, file)
-                zipf.write(file_path, os.path.relpath(file_path, source_dir))
+                rel_path = os.path.relpath(file_path, source_dir)
+                zipf.write(file_path, rel_path)
     print("File create success")
 
 
